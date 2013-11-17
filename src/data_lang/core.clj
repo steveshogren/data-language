6f01a3a98e6b6c7af614c01b53122e95ed9da000
Steve Shogren <steve.a.shogren@gmail.com>
2013-11-17 12:23:15 -0500

    Playing around with some basic exprs

--- src/data_lang/core.clj
+++ src/data_lang/core.clj
@@ -1,6 +1,32 @@
 (ns data-lang.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn make-stmts []
+  [{:fn-id 2 :arg1 2 :arg2 2}
+   {:fn-id 1 :arg1 1 :arg2 1}])
+
+(defn make-fns []
+  [{:fn-id 1 :map 'map}
+   {:fn-id 1 :map '+}])
+
+(defn lookup-fn [id fns]
+  (:map (first (filter #(= id (:fn-id %)) fns))))
+
+(defn foo [stmts fns]
+  (map
+   (fn [x]
+     (list (lookup-fn (:fn-id x) fns)
+           (:arg1 x)
+           (:arg2 x)))
+   stmts))
+
+(foo (make-stmts) (make-fns))
+
+
+(defn mapper [fnk x]
+  (defn inner [vals]
+    (if (empty? vals) '()
+        (conj (inner (rest vals))
+              (fnk (first vals)))))
+  (inner x))
+
+(mapper (fn [z] (+ z 1)) [1 2 3 4 5]) 
