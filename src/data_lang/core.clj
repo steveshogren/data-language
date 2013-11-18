4f0bca3ff98dbc7cb368aeef576484809c156a61
Steve Shogren <steve.a.shogren@gmail.com>
2013-11-18 16:12:37 -0500

    Adding a function define normalizer

--- src/data_lang/core.clj
+++ src/data_lang/core.clj
@@ -30,3 +30,29 @@
   (inner x))
 
 (mapper (fn [z] (+ z 1)) [1 2 3 4 5]) 
+
+(defn normalize [denorms]
+  (if (list? denorms) 
+    ;; is Expr
+    (let [func (first denorms)
+          args (rest denorms)]
+      (cond
+       (= 'define func)
+       (let [[name & fargs] (first args)
+             body (rest args)]
+         {:function name :args fargs :body (map normalize body)})
+       (= '+ func) {:expr `+ :args (map normalize args)}
+       :else :fail))
+    ;; if primitive...
+    denorms))
+
+(defn normalize-all [denorm-list]
+  (map normalize denorm-list))
+
+(normalize-all
+ '((define (adder x y)
+     (+ x y))
+   (adder 1 2)))
+
+
+
