c2f7ac5b6622248ba749cdb5f8549f2570d0f62d
Steve Shogren <steve.a.shogren@gmail.com>
2013-11-19 17:15:59 -0500

    Round tripping a basic implementation

--- src/data_lang/core.clj
+++ src/data_lang/core.clj
@@ -42,12 +42,34 @@
              body (rest args)]
          {:function name :args fargs :body (map normalize body)})
        (= '+ func) {:expr `+ :args (map normalize args)}
-       :else :fail))
+       :else {:expr func :args (map normalize args)}))
     ;; if primitive...
     denorms))
 
 (defn normalize-all [denorm-list]
   (map normalize denorm-list))
+
+(defn denormalize [norms]
+  (if (map? norms) 
+    ;; is Expr
+    (cond
+     (contains? norms :function)
+     (let [func (:function norms)
+           args (:args norms)
+           body (:body norms)]
+       `(~'define (~func ~args) ~@(map denormalize body)))
+     (contains? norms :expr)
+     (let [expr (:expr norms)
+           args (:args norms)]
+       `(~expr ~@args)))
+    ;; if primitive...
+    norms))
+
+(defn denormalize-all [norm-list]
+  (map denormalize norm-list))
+
+(denormalize-all '({:function adder, :args (x y), :body ({:expr clojure.core/+, :args (x y)})}
+                   {:expr adder, :args (1 2)}))
 
 (normalize-all
  '((define (adder x y)
--- test/data_lang/core_test.clj
+++ test/data_lang/core_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [data-lang.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest normalize-test
+  (testing "Normalize function"
+    (is (= (denormalize-all (normalize-all
+                             '((define (adder x y)
+                                 (+ x y))
+                               (adder 1 2))))
+           '((define (adder x y)
+               (+ x y))
+             (adder 1 2))))))
+
