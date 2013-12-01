fb98ef81ce43c6d0113019f93c088d1d0e523834
Steve Shogren <steve.a.shogren@gmail.com>
2013-12-01 15:03:11 -0500

    Adding a basic rename refactoring

--- src/data_lang/core.clj
+++ src/data_lang/core.clj
@@ -9,16 +9,33 @@
   '([scheme.+ + 2]
       [scheme.- - 2]))
 
-(defn round-trip [filename]
+(def store-name "test.edn")
+(def filename "test.rkt")
+
+(defn round-trip []
   (let [denorm-input  (io/read-data filename)
         normalized (n/normalize-all denorm-input language-mappings)]
     (do 
-      (io/write-edn normalized "test.edn")
-      (let [norm-from-store (io/read-edn "test.edn")
+      (io/write-edn normalized store-name)
+      (let [norm-from-store (io/read-edn store-name)
             denormalized (d/denormalize-all norm-from-store language-mappings)]
         (io/write-data denormalized filename)))))
 
-(round-trip "test.rkt")
+(defn renam [data name new-name]
+  (map
+   #(if (and (contains? % :function)
+             (= name (:function %)))
+      (assoc % :function new-name)
+      %)
+   data))
+
+(defn rename-global-symbol [name new-name]
+  (let [data (io/read-edn store-name)
+        data (renam data name new-name)
+        denormalized (d/denormalize-all data language-mappings)]
+    (do
+      (io/write-edn data store-name)
+      (io/write-data denormalized filename))))
 
 (defn -main
   [filename & args]
