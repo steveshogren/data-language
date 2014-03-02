03137ca1f0debbf257a1006e4800b7d0d1b44e97
Steve Shogren <steve.a.shogren@gmail.com>
2014-03-01 19:26:16 -0500

    Refactor test to be more readable

--- test/data_lang/core_test.clj
+++ test/data_lang/core_test.clj
@@ -17,24 +17,22 @@ data-lang.core/language-mappings
   (let [[normed _] (n/normalize-all code data-lang.core/language-mappings)]
     normed))
 
+(def sample-edn
+  '[{:id adderX,
+     :function adder,
+     :params ({:id adder.xX, :name x} {:id adder.yX, :name y}),
+     :body ({:expr clojure.+, :args (adder.xX adder.yX)})}
+    {:expr adderX, :args (1 2)}])
+
 (deftest normalize-test
   (testing "Normalize function"
-    (is (=
-         (with-redefs [gensym (fn [x] (symbol (str x "X")))]
-           (norm '((defn adder [x y]
-                     (+ x y))
-                   (adder 1 2))))
-         '[{:id adderX, :function adder, :params ({:id adder.xX, :name x} {:id adder.yX, :name y}), :body ({:expr clojure.+, :args (adder.xX adder.yX)})} {:expr adderX, :args (1 2)}]
-           ))
-
-    (is (=
-           (denorm '[{:id adderX, :function adder, :params ({:id adder.xX, :name x} {:id adder.yX, :name y}), :body ({:expr clojure.+, :args (adder.xX adder.yX)})} {:expr adderX, :args (1 2)}])
-           '[(defn adder [x y] (+ x y)) (adder 1 2)]
-         ))
-    (is (=
-         (round '[(defn adder [x y] (+ x y)) (adder 1 2)])
-         '[(defn adder [x y] (+ x y)) (adder 1 2)]
-         ))
+    (is (= (with-redefs [gensym (fn [x] (symbol (str x "X")))]
+             (norm '((defn adder [x y] (+ x y)) (adder 1 2))))
+           sample-edn))
+    (is (= (denorm sample-edn)
+           '[(defn adder [x y] (+ x y)) (adder 1 2)]))
+    (is (= (round '[(defn adder [x y] (+ x y)) (adder 1 2)])
+           '[(defn adder [x y] (+ x y)) (adder 1 2)]))
     ))
 
 
